09cd72a4530e6595f1838335ac28094b11dd8bcf
xindong.xie <779788582@qq.com>
2020-01-04 15:36:02 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React , {Component} from "react"
+import {Button} from "antd";
+import "./app.less"
+
+class App extends Component{
+    render() {
+        return<div>
+            <Button type={"primary"}>确定</Button>
+        </div>
+    }
+}
+
+export default App
